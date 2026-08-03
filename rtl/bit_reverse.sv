f5f15b40e67a7165d1c0c84eaff1c467397ec488
ankitannasamudram <ankit2906.a@gmail.com>
2026-08-02 21:38:53 -0700

    Implemented parameterized bit reversal module

--- rtl/bit_reverse.sv
+++ rtl/bit_reverse.sv
@@ -4,5 +4,19 @@ module bit_reverse #(
     input  logic [WIDTH-1:0] index_in,
     output logic [WIDTH-1:0] index_out
 );
-    // TODO: combinational bit reversal.
+    //Simple for loop for bit reversal for variable WIDTH
+   
+
+    integer i;
+
+
+always_comb begin
+    for (i= 0; i<WIDTH; i=i+1) begin 
+     index_out[i] = index_in[(WIDTH-1)-i];
+
+
+    end 
+
+    end
+
 endmodule
